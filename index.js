8c3a5a072ffdc8f6055bfb686ca4675749252393
gonzalo <gonzalo.a.gomez22@gmail.com>
2022-04-06 12:25:12 -0300

    correctly display player and computer choices, the winner of 5 rounds and reset the scores

--- index.js
+++ index.js
@@ -1,87 +1,75 @@
 let hands = ["rock", "paper", "scissors"]
 let playerScore = 0
 let computerScore = 0
+let scoreEl = document.getElementById("score-el");
+scoreEl.textContent = "Player Score: " + playerScore + " " + "Computer Score: " + computerScore
+let rndChoices = document.getElementById("rndChoices");
+let playerSelection = ''
+let choicesEl = document.getElementById("choices-el")
+const buttons = document.querySelectorAll("#btn")
+
 
 // // function that makes the computer play 1 of 3 hands
 function computerPlay() {
     let randomIndex = Math.floor(Math.random() * 3)
     return hands[randomIndex]
 }
-let playerSelection = ''
-const buttons = document.querySelectorAll("#btn")
 
-// // add click listener in each button, get the text inside the buttons, play 1 round of rps
+// // add click listener in each button, get the text inside the buttons, play 1 round of rps + update scores
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
 
         playerSelection = e.target.innerText.toLowerCase()
         playRound()
+        scoreEl.textContent = "Player Score: " + playerScore + " " + "Computer Score: " + computerScore
+        decideWinner()
     })
 })
-
-// // function that gets the user input
-function userInput() {
-    return playerSelection
-}
 
 // // function that plays a single round of Rock Paper Scissors. 
 function playRound() {
     let computerSelection = computerPlay()
-    console.log(playerSelection)
-    playerSelection = userInput()
-    console.log(computerSelection)
+    choicesEl.textContent = "Player choice: " + playerSelection + " " + "Computer choice: " + computerSelection
+    // console.log(playerSelection)
+    // console.log(computerSelection)
     if (playerSelection === "rock" && computerSelection === "rock") {
-        return "it's a tie(Rock)"
+        return rndChoices.textContent = "it's a tie(Rock)"
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         computerScore = ++computerScore
-        return "You lost! Paper beats Rock!"
+        return rndChoices.textContent = "You lost! Paper beats Rock!"
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
         playerScore = ++playerScore
-        return "You win! Rock beats Scissors!"
+        return rndChoices.textContent = "You win! Rock beats Scissors!"
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         playerScore = ++playerScore
-        return "You win! Paper beats Rock!"
+        return rndChoices.textContent = "You win! Paper beats Rock!"
     } else if (playerSelection === "paper" && computerSelection === "paper") {
-        return "it's a tie(Paper)"
+        return rndChoices.textContent = "it's a tie(Paper)"
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
         computerScore = ++computerScore
-        return "You lost! Scissors beats Paper!"
+        return rndChoices.textContent = "You lost! Scissors beats Paper!"
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         computerScore = ++computerScore
-        return "You lost! Rock beats Scissors!"
+        return rndChoices.textContent = "You lost! Rock beats Scissors!"
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         playerScore = ++playerScore
-        return "You win! Scissors beats Paper!"
+        return rndChoices.textContent = "You win! Scissors beats Paper!"
     } else if (playerSelection === "scissors" && computerSelection === "scissors") {
-        return "it's a tie(Scissors)"
+        return rndChoices.textContent = "it's a tie(Scissors)"
     }
+
+
 }
 
-// function that decides the winner of the round
+// function that decides the winner of the round and resets the scores
 function decideWinner() {
-    if (playerScore > computerScore) {
-        console.log("You won the round!")
-    } else if (playerScore < computerScore) {
-        console.log("You lost the round!")
-    } else {
-        console.log("This round is a tie!")
+    if (playerScore === 5) {
+        rndChoices.textContent = ("You won the round!")
+        playerScore = 0
+        computerScore = 0
+    } else if (computerScore === 5) {
+        rndChoices.textContent = ("You lost the round!")
+        computerScore = 0
+        playerScore = 0
     }
 }
-
-
-
-// let scoreEl = document.getElementById("score-el");
-// scoreEl.textContent = "Player Score: " + playerScore + " " + "Computer Score: " + computerScore
-
-// function that creates 5 rounds of rps 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        // userInput()
-        console.log(playRound())
-        console.log("Player: " + playerScore + " " + "Computer: " + computerScore)
-    }
-    decideWinner()
-    playerScore = 0
-    computerScore = 0
-}
-
